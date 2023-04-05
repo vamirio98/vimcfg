34c7cf144334c2f901399f3e6c12d19e8e389008
vamirio <vamirio@qq.com>
2023-04-05 16:04:48 +0800

    add style file for Lua

--- utils/set_code_style.py
+++ utils/set_code_style.py
@@ -1,30 +1,61 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#-
+# -
 # set_code_style.py
 #
 # Created by vamirio on 2022 Aug 30
-#-
+# -
 
 import sys
-import subprocess
+import shutil
 import os
 
 import pylib.base as base
 
-if not base.checkArgsNum(sys.argv, 2, "Args: srcStyleFile curDir."):
-    exit(1)
 
-srcStyleFile, curDir = sys.argv[1:]
-projectRoot = base.getProjectRoot(curDir)
-if projectRoot == "":
-    base.printError("Error: can't find the root directory of project, check if a",
-            base.projectRootFlag, "file/directory in it.")
-    exit(1)
+def setDestFile(language: str) -> str:
+    destFile = ""
+    if language == "c" or language == "cpp":
+        destFile = ".clang-format"
+    elif language == "lua":
+        destFile = ".stylua.toml"
+    else:
+        pass
 
-destStyleFile = os.path.join(projectRoot, ".clang-format")
-if os.path.exists(destStyleFile):
-    base.printError("Warn:", destStyleFile,
-          "is already exists, remove it if you want to reset the code style.")
-    exit(1)
-subprocess.run(["cp", srcStyleFile, destStyleFile])
+    return destFile
+
+
+def main():
+    if not base.checkArgsNum(sys.argv, 3, "Args: srcFile curDir language."):
+        exit(1)
+
+    srcFile, curDir, language = sys.argv[1:]
+
+    projectRoot = base.getProjectRoot(curDir)
+    if projectRoot == "":
+        base.printError(
+            "Error: can't find the root directory of project, check if a",
+            base.projectRootFlag,
+            "file/directory in it.",
+        )
+        exit(1)
+
+    fileName = setDestFile(language)
+    if fileName == "":
+        base.printError("Error: unknown language:", language)
+        exit(1)
+
+    destFile = os.path.join(projectRoot, fileName)
+    if os.path.exists(destFile):
+        base.printError(
+            "Warn:",
+            destFile,
+            "is already exists, remove it if you want to reset the code style.",
+        )
+        exit(1)
+
+    shutil.copyfile(srcFile, destFile)
+
+
+if __name__ == '__main__':
+    main()
