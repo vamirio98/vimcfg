6848c0ec8f04406927d3ea80992c0f0108f54891
vamirio <vamirio@qq.com>
2025-02-16 23:25:23 +0800

    add '[+]' for modified file

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -45,8 +45,11 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
+      local icons = LazyVim.config.icons
+
       opts.options.component_separators = { left = "", right = "" }
       opts.options.section_separators = { left = "", right = "" }
+      opts.sections.lualine_c[4] = { LazyVim.lualine.pretty_path({ modified_sign = " [+]" }) }
     end,
   },
 
